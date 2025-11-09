8c5b091495626c235ce901e16dafbfe41a538486
PamplemoussRose <95235051+PamplemoussRose@users.noreply.github.com>
2025-11-08 19:28:19 -0500

    V1.0

---
+++ fr/js/script.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Pour la page Formations
+    const educationListItems = document.querySelectorAll('.education-list ul li');
+    const educationDetailItems = document.querySelectorAll('.education-detail .detail-item');
+
+    educationListItems.forEach(item => {
+        item.addEventListener('click', function() {
+            const targetId = this.getAttribute('data-education');
+            educationDetailItems.forEach(detail => {
+                if (detail.id === targetId) {
+                    detail.style.display = 'block';
+                } else {
+                    detail.style.display = 'none';
+                }
+            });
+        });
+    });
+
+    // Pour la page Expériences
+    const experienceListItems = document.querySelectorAll('.experience-list ul li');
+    const experienceDetailItems = document.querySelectorAll('.experience-detail .detail-item');
+
+    experienceListItems.forEach(item => {
+        item.addEventListener('click', function() {
+            const targetId = this.getAttribute('data-experience');
+            experienceDetailItems.forEach(detail => {
+                if (detail.id === targetId) {
+                    detail.style.display = 'block';
+                } else {
+                    detail.style.display = 'none';
+                }
+            });
+        });
+    });
+});
+
+window.onbeforeunload = () => {
+  for(const form of document.getElementsByTagName('form')) {
+    form.reset();
+  }
+}
